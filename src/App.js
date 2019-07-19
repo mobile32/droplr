c4f2f4d3841f0fe6b877cc7f7e80cc3d953c3b77
Grzegorz Zaręba <g.zareba@selleo.com>
2019-07-19 15:27:18 +0200

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./stylesheets/application.sass";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="nav">
+        <div className="nav__logo">Logo</div>
+        <ul className="nav__links">
+          <li className="nav__link">
+            <a href="#features">Features</a>
+          </li>
+          <li className="nav__link">
+            <a href="#apps">Apps</a>
+          </li>
+          <li className="nav__link">
+            <a href="#pricing">Pricing</a>
+          </li>
+          <li className="nav__link">
+            <a href="#knowledge-base">Knowledge base</a>
+          </li>
+          <li className="nav__link">
+            <a href="#business">Business</a>
+          </li>
+        </ul>
+        <div className="nav__buttons">
+          <div className="nav__button">Log in</div>
+          <div className="nav__button">Sign up</div>
+        </div>
+      </div>
     </div>
   );
 }
