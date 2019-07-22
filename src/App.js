d8169d3d6c1cd7b57187a69aa822ff41739a2802
Grzegorz Zaręba <g.zareba@selleo.com>
2019-07-22 17:10:47 +0200

    Nav gradients

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <div className="nav">
-        <div className="nav__logo">Logo</div>
+        <div className="nav__logo">LOGO</div>
         <ul className="nav__links">
           <li className="nav__link">
             <a href="#features">Features</a>
@@ -28,7 +28,10 @@ function App() {
           <div className="nav__button -primary">Sign up</div>
         </div>
       </div>
-      <div className="content" />
+      <div className="subscribe">
+        <div className="subscribe__animation">left</div>
+        <div className="subscribe__start">right</div>
+      </div>
     </div>
   );
 }
