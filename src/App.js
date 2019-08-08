bb67f17f54aa12d4dc7dde799644bd99e4181f41
Grzegorz Zaręba <g.zareba@selleo.com>
2019-08-08 18:48:30 +0200

    Slider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./stylesheets/application.sass";
 import logo from "./images/logo.png";
 import woman from "./images/businesswoman-portrait-black-top-grey-background.jpg";
+import { Play, RightQute, LeftQoute, RightQuote } from "./images/Icons";
 
 function App() {
   return (
@@ -33,21 +34,7 @@ function App() {
       <div className="subscribe">
         <div className="subscribe__animation-container">
           <div className="subscribe__animation">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="caret-right"
-              className="subscribe__icon"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 192 512"
-            >
-              <path
-                fill="currentColor"
-                d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
-              />
-            </svg>
+            <Play />
           </div>
         </div>
         <div className="subscribe__start">
@@ -89,14 +76,18 @@ function App() {
           <div className="testimonials__arrow">{"<"}</div>
           <div className="testimonials__left-panel">
             <div className="testimonials__quotation">
+              <LeftQoute className="testimonials__left-quote" />
               <h4>Great for sharing screenshots!</h4>
               <p>
                 Dropl is the simplest, most reliable, universal way to share a
                 file with anyone anywhere, I cant't imagine a quicker way to
                 exchange a screenshot than dropl.
               </p>
-              <p>Des Traynor</p>
-              <span>Co-founder @Intercom</span>
+              <div>
+                <b>Des Traynor</b>
+              </div>
+              <div className="testimonials__role">Co-founder @Intercom</div>
+              <RightQuote className="testimonials__right-quote"/>
             </div>
             <div className="testimonials__buttons">
               <div className="testimonials__button -active" />
---
+++ src/images/Icons.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+export const Play = ({ className }) => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      className={className}
+      data-prefix="fas"
+      data-icon="caret-right"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 192 512"
+    >
+      <path
+        fill="currentColor"
+        d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"
+      />
+    </svg>
+  );
+};
+
+export const RightQuote = ({ className }) => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      className={className}
+      data-prefix="fas"
+      data-icon="quote-right"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="currentColor"
+        d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"
+      />
+    </svg>
+  );
+};
+
+export const LeftQoute = ({ className }) => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      className={className}
+      data-prefix="fas"
+      data-icon="quote-left"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="currentColor"
+        d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+      />
+    </svg>
+  );
+};
